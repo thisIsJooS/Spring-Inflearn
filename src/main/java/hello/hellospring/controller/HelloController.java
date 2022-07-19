b0c1c3c814353d76c0e048da45aea127125b759c
thisIsJooS <thisis.joos@gmail.com>
2022-07-19 23:13:26 +0900

    ~section4.1

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
@@ -25,6 +24,26 @@ public class HelloController {
     @ResponseBody
     public String helloString(@RequestParam("name") String name){
         return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
